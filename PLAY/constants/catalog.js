1ce13c6db1a12c3b4b96b1b3aedf58f388ab8b21
Kurmash1212 <167734258+Kurmash1212@users.noreply.github.com>
2024-04-22 10:24:42 +0600

    Add files via upload

---
+++ PLAY/constants/catalog.js
@@ -0,0 +1,56 @@
+const CATALOG = [{
+    id: "el1",
+    name: "Lenovo",
+    img: "https://object.pscloud.io/cms/cms/Photo/img_0_62_1913_1_6.png",
+    price: 43000
+},
+{
+    id: "el2",
+    name: "Lenovo",
+    img: "https://object.pscloud.io/cms/cms/Photo/img_0_62_2124_0_6.png",
+    price: 45000
+},
+{
+    id: "el3",
+    name: "Lenovo",
+    img: "https://object.pscloud.io/cms/cms/Photo/img_0_62_2340_1_6.png",
+    price: 40000
+},
+{
+    id: "el4",
+    name: "Acer",
+    img: "https://object.pscloud.io/cms/cms/Photo/img_0_62_2644_0_1.jpg",
+    price: 55000
+},
+{
+    id: "el5",
+    name: "Macbook",
+    img: "https://object.pscloud.io/cms/cms/Photo/img_0_62_1075_6.jpg",
+    price: 80000
+},
+{
+    id: "el6",
+    name: "Lenovo",
+    img: "https://softech.kg/image/cache/b86c89d413b234a104c9a2d714be3832.jpg",
+    price: 40000
+},
+{
+    id: "el7",
+    name: "Acer",
+    img: "https://object.pscloud.io/cms/cms/Photo/img_0_62_2680_4_6.png",
+    price: 65000
+},
+{
+    id: "el8",
+    name: "Macbook",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUlo0pgYr_ZSql2ei0NelF3iEy2K7Uov5sRN-SLZGDdA&s",
+    price: 100000
+},
+{
+    id: "el9",
+    name: "Dell",
+    img: "https://ultra.kg/upload/resize_cache/iblock/926/800_800_1d0e97ea46f4438969ab06dd5b311ca67/926ff3c568ce3bb67433da00e94c0a31.jpg",
+    price: 70000
+}
+
+]
